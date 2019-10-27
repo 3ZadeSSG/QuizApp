aefadf8d949b960422a07bc6e570ea8f30e5471f
Rohan Patil <prorohan8@gmail.com>
2019-10-27 12:56:41 +0530

    adding material design ui fixes

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     All these RadioGroup, CheckBox etc. will be initialized in the onCreate method, so this way more optimized since all
     views will be initialized once.
     */
-    CheckBox question1_a,question1_b,question1_c,question1_d;
+    CheckBox question1_a, question1_b, question1_c, question1_d;
     RadioGroup question2;
     RadioButton question2_answer;
     RadioGroup question3;
@@ -39,37 +39,39 @@ public class MainActivity extends AppCompatActivity {
     RadioGroup question9;
     RadioButton question9_answer;
     EditText question_10;
-    int score=0;
+    int score = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        question1_a=findViewById(R.id.check_question_1_a);
-        question1_b=findViewById(R.id.check_question_1_b);
-        question1_c=findViewById(R.id.check_question_1_c);
-        question1_d=findViewById(R.id.check_question_1_d);
-        question2=findViewById(R.id.radiogrp_2);
-        question2_answer=findViewById(R.id.radio_question_2_d);
-        question3=findViewById(R.id.radiogrp_3);
-        question3_answer=findViewById(R.id.radio_question_3_d);
-        question4=findViewById(R.id.radiogrp_4);
-        question4_answer=findViewById(R.id.radio_question_4_b);
-        question5=findViewById(R.id.radiogrp_5);
-        question5_answer=findViewById(R.id.radio_question_5_b);
-        question6=findViewById(R.id.radiogrp_6);
-        question6_answer=findViewById(R.id.radio_question_6_a);
-        question7=findViewById(R.id.radiogrp_7);
-        question7_answer=findViewById(R.id.radio_question_7_b);
-        question8=findViewById(R.id.radiogrp_8);
-        question8_answer=findViewById(R.id.radio_question_8_c);
-        question9=findViewById(R.id.radiogrp_9);
-        question9_answer=findViewById(R.id.radio_question_9_c);
-        question_10=findViewById(R.id.question_10_text);
+        question1_a = findViewById(R.id.check_question_1_a);
+        question1_b = findViewById(R.id.check_question_1_b);
+        question1_c = findViewById(R.id.check_question_1_c);
+        question1_d = findViewById(R.id.check_question_1_d);
+        question2 = findViewById(R.id.radiogrp_2);
+        question2_answer = findViewById(R.id.radio_question_2_d);
+        question3 = findViewById(R.id.radiogrp_3);
+        question3_answer = findViewById(R.id.radio_question_3_d);
+        question4 = findViewById(R.id.radiogrp_4);
+        question4_answer = findViewById(R.id.radio_question_4_b);
+        question5 = findViewById(R.id.radiogrp_5);
+        question5_answer = findViewById(R.id.radio_question_5_b);
+        question6 = findViewById(R.id.radiogrp_6);
+        question6_answer = findViewById(R.id.radio_question_6_a);
+        question7 = findViewById(R.id.radiogrp_7);
+        question7_answer = findViewById(R.id.radio_question_7_b);
+        question8 = findViewById(R.id.radiogrp_8);
+        question8_answer = findViewById(R.id.radio_question_8_c);
+        question9 = findViewById(R.id.radiogrp_9);
+        question9_answer = findViewById(R.id.radio_question_9_c);
+        question_10 = findViewById(R.id.question_10_text);
 
 
     }
+
     //Reset methods will clear all options when RESET button pressed
-    public void reset(View view){
+    public void reset(View view) {
         question1_a.setChecked(false);
         question1_b.setChecked(false);
         question1_c.setChecked(false);
@@ -83,44 +85,42 @@ public class MainActivity extends AppCompatActivity {
         question8.clearCheck();
         question9.clearCheck();
         question_10.getText().clear();
-        score=0;
+        score = 0;
     }
-    public void submit(View view){
-        if(question2_answer.isChecked()){
+
+    public void submit(View view) {
+        if (question2_answer.isChecked()) {
             score++;
         }
-        if(question3_answer.isChecked()){
+        if (question3_answer.isChecked()) {
             score++;
         }
-        if(question4_answer.isChecked()){
+        if (question4_answer.isChecked()) {
             score++;
         }
-        if(question5_answer.isChecked()){
+        if (question5_answer.isChecked()) {
             score++;
         }
-        if(question6_answer.isChecked()){
+        if (question6_answer.isChecked()) {
             score++;
         }
-        if(question7_answer.isChecked()){
+        if (question7_answer.isChecked()) {
             score++;
         }
-        if(question8_answer.isChecked()){
+        if (question8_answer.isChecked()) {
             score++;
         }
-        if(question9_answer.isChecked()){
+        if (question9_answer.isChecked()) {
             score++;
         }
-        if(question1_a.isChecked() && question1_b.isChecked() && question1_c.isChecked() && !question1_d.isChecked()){
+        if (question1_a.isChecked() && question1_b.isChecked() && question1_c.isChecked() && !question1_d.isChecked()) {
             score++;
         }
-        String s=question_10.getText().toString();
+        String s = question_10.getText().toString();
         //string is compared with uppercase because user can input in amy order
-        if(s.toUpperCase().equals("DECISION TREE")){
+        if (s.toUpperCase().equals("DECISION TREE")) {
             score++;
         }
-      /*  Toast.makeText(MainActivity.this,
-                "Your total: "+score, Toast.LENGTH_LONG).show();
-                */
       /*
       New intent to launch the share screen
        */
